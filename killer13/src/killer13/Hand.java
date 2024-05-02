26912e9f4c35a07a5829b90baaf44f594a09d250
jyd647 <110570650+jyd647@users.noreply.github.com>
2024-05-02 11:57:03 -0700

    added buttons for home screen, fix to make more functional and added to screen

--- killer13/src/killer13/Hand.java
+++ killer13/src/killer13/Hand.java
@@ -2,6 +2,7 @@ package killer13;
 
 import java.util.ArrayList;
 
+
 public class Hand {
 	private ArrayList<Card> curr;
 	public static void main(String[] args) {
--- killer13/src/killer13/Player.java
+++ killer13/src/killer13/Player.java
@@ -1,10 +1,84 @@
 package killer13;
-
-public class Player {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.*;
+import javax.swing.*;
+public class Player extends JPanel implements ActionListener{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+		}
+
+	protected ImageIcon createImageIcon(String path,
+            String description) {
+		java.net.URL imgURL = getClass().getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL, description);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
+	}
+	protected static ImageIcon createImageIcon(String path) {
+	    java.net.URL imgURL = Player.class.getResource(path);
+	   //error handling omitted for clarity...
+	    return new ImageIcon(imgURL);
+	}
+	//setting up buttons for home screen
+	ImageIcon rulesButton = createImageIcon("table.jpg", "help");
+	ImageIcon playButton = createImageIcon("table.jpg", "me");
+	ImageIcon exitButton = createImageIcon("table.jpg", "pls");
+	{
+	
+	JButton b1 = new JButton("?", rulesButton);
+	b1.setVerticalTextPosition(AbstractButton.CENTER);
+	b1.setHorizontalTextPosition(AbstractButton.LEADING); //aka LEFT, for left-to-right locales
+	b1.setMnemonic(KeyEvent.VK_D); //what does this mean
+	b1.setActionCommand("rules"); // the command it follows
+	
+	//play button
+	JButton b2 = new JButton("PLAY", playButton);
+	b2.setVerticalTextPosition(AbstractButton.BOTTOM);
+	b2.setHorizontalTextPosition(AbstractButton.CENTER);
+	b2.setMnemonic(KeyEvent.VK_M);
+	
+	JButton b3 = new JButton("EXIT", exitButton);
+	//Use the default text position of CENTER, TRAILING (RIGHT).
+	b3.setMnemonic(KeyEvent.VK_E);
+	b3.setActionCommand("exit");
+	b3.setEnabled(false);
+	
+	//Listen for actions on buttons 1 and 3.
+	b1.addActionListener(this);
+	b3.addActionListener(this);
+	
+//	b1.setToolTipText("Click this button to disable " // hover screens
+//	                  + "the middle button.");
+//	b2.setToolTipText("This middle button does nothing "
+//	                  + "when you click it.");
+//	b3.setToolTipText("Click this button to enable the "
+//	                  + "middle button.");
 
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if ("rules".equals(e.getActionCommand())) {
+		} else if ("play".equals(e.getActionCommand())) {
+	
+		} else {
+		
+		}
+}
 }
