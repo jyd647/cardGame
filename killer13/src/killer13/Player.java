345648f4edc5f1e792231f1205c2c740a5639313
jyd647 <110570650+jyd647@users.noreply.github.com>
2024-05-06 11:59:18 -0700

    added buttons to gui, to fix size and add background

--- killer13/src/killer13/Player.java
+++ killer13/src/killer13/Player.java
@@ -2,6 +2,7 @@ package killer13;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -19,24 +20,36 @@ public class Player extends JPanel implements ActionListener{
 	
 	public void paint(Graphics g) {
 		super.paintComponent(g);
-		System.out.println("WORK");
-
-		enter.paint(g);
+		///g.drawImage
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Player f = new Player();
 	}
 	public Player () {
-		JFrame f = new JFrame("killer");
+		JFrame fr = new JFrame("killer");
 		
-		f.setVisible(true);
-		f.setSize(new Dimension(width, height));
-		f.setBackground(Color.black);
-		f.add(this);
-		f.setResizable(true);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		fr.setSize(617, 360);
+		fr.setResizable(true);
+		
+		ImageIcon image1 = new ImageIcon("table.jpg");
+        fr.add(new JLabel(image1));
+        fr.pack();        
+        JButton rules = new JButton("RULES"); 
+        rules.setPreferredSize(new Dimension (5, 5));
+        rules.setHorizontalAlignment(SwingConstants.LEFT);
+        JButton play = new JButton("PLAY"); 
+        play.setPreferredSize(new Dimension (5, 5));
+        play.setHorizontalAlignment(SwingConstants.CENTER);
+        JButton exit = new JButton("EXIT"); 
+        exit.setPreferredSize(new Dimension (5, 5));
+        exit.setHorizontalAlignment(SwingConstants.RIGHT);
+        fr.getContentPane().add(rules);
+        fr.getContentPane().add(play);
+        fr.getContentPane().add(exit);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fr.setVisible(true);
 	}
 	protected ImageIcon createImageIcon(String path,
             String description) {
@@ -54,43 +67,38 @@ public class Player extends JPanel implements ActionListener{
 	    return new ImageIcon(imgURL);
 	}
 	//setting up buttons for home screen
-	ImageIcon rulesButton = createImageIcon("table.jpg", "help");
-	ImageIcon playButton = createImageIcon("table.jpg", "me");
-	ImageIcon exitButton = createImageIcon("table.jpg", "pls");
-	{
-	
-	JButton b1 = new JButton("?", rulesButton);
-	b1.setVerticalTextPosition(AbstractButton.CENTER);
-	b1.setHorizontalTextPosition(AbstractButton.LEADING); //aka LEFT, for left-to-right locales
-	b1.setMnemonic(KeyEvent.VK_D); //what does this mean
-	b1.setActionCommand("rules"); // the command it follows
-	
-	//play button
-	JButton b2 = new JButton("PLAY", playButton);
-	b2.setVerticalTextPosition(AbstractButton.BOTTOM);
-	b2.setHorizontalTextPosition(AbstractButton.CENTER);
-	b2.setMnemonic(KeyEvent.VK_M);
-	
-	
-	//exit button
-	JButton b3 = new JButton("EXIT", exitButton);
-	//Use the default text position of CENTER, TRAILING (RIGHT).
-	b3.setMnemonic(KeyEvent.VK_E);
-	b3.setActionCommand("exit");
-	b3.setEnabled(false);
-	
-	//Listen for actions on buttons 1 and 3.
-	b1.addActionListener(this);
-	b3.addActionListener(this);
-	
-//	b1.setToolTipText("Click this button to disable " // hover screens
-//	                  + "the middle button.");
-//	b2.setToolTipText("This middle button does nothing "
-//	                  + "when you click it.");
-//	b3.setToolTipText("Click this button to enable the "
-//	                  + "middle button.");
+//	ImageIcon rulesButton = createImageIcon("table.jpg", "help");
+//	ImageIcon playButton = createImageIcon("table.jpg", "me");
+//	ImageIcon exitButton = createImageIcon("table.jpg", "pls");
+//	{
+//	
+//	JButton b1 = new JButton("?", rulesButton);
+//	b1.setVerticalTextPosition(AbstractButton.CENTER);
+//	b1.setHorizontalTextPosition(AbstractButton.LEADING); //aka LEFT, for left-to-right locales
+//	b1.setMnemonic(KeyEvent.VK_D); //what does this mean
+//	b1.setActionCommand("rules"); // the command it follows
+//	b1.setVisible(true);
+//	
+//	//play button
+//	JButton b2 = new JButton("PLAY", playButton);
+//	b2.setVerticalTextPosition(AbstractButton.BOTTOM);
+//	b2.setHorizontalTextPosition(AbstractButton.CENTER);
+//	b2.setMnemonic(KeyEvent.VK_M);
+//	b2.setVisible(true);
+//	
+//	//exit button
+//	JButton b3 = new JButton("EXIT", exitButton);
+//	//Use the default text position of CENTER, TRAILING (RIGHT).
+//	b3.setMnemonic(KeyEvent.VK_E);
+//	b3.setActionCommand("exit");
+//	b3.setEnabled(false);
+//	b3.setVisible(true);
+//	//Listen for actions on buttons 1 and 3.
+//	b1.addActionListener(this);
+//	b3.addActionListener(this);
+//	
 
-	}
+	//}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
