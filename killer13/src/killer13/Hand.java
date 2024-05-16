b0424b434d1b9d038967b2da27aaafda183eec0c
1943955 <1943955@01-19643.pusd.dom>
2024-05-16 11:56:34 -0700

    started Hand class

--- killer13/src/killer13/Hand.java
+++ killer13/src/killer13/Hand.java
@@ -5,9 +5,18 @@ import java.util.ArrayList;
 
 public class Hand {
 	private ArrayList<Card> curr;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private ArrayList<Card> combos;
+	
+	
+	public Hand() {
+		
+		combos = new ArrayList<Card>();
+		
+		
+	}
+	
+	public ArrayList<Card> combos(){
+		return combos;
 	}
 
 }
