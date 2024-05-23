12e39aa00d6a47c303964f63d5fc4ce711dec75f
1943955 <1943955@01-19643.pusd.dom>
2024-05-23 11:13:53 -0700

    print console to JPanel

---
+++ killer13/src/killer13/ConsoleView.java
@@ -0,0 +1,95 @@
+package killer13;
+
+import java.awt.BorderLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class ConsoleView extends JFrame {
+	
+	public ConsoleView() {
+		this.setLocation(100,100);
+		this.setSize(600,400);
+		this.setLayout(new BorderLayout());
+		//this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JLabel header = new JLabel("Console");
+		this.add(header, BorderLayout.NORTH);
+		
+		final JTextArea console = new JTextArea();
+		console.setEditable(false);
+		this.add(new JScrollPane(console), BorderLayout.CENTER);
+		
+		final JTextField inputField = new JTextField();
+		
+		JButton enterButton = new JButton("Enter");
+		enterButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(inputField.getText());
+				inputField.setText(null);
+			}
+		});
+		
+		this.getRootPane().setDefaultButton(enterButton);
+	
+		JPanel inputFieldPanel = new JPanel();
+		GroupLayout layout = new GroupLayout(inputFieldPanel);
+		inputFieldPanel.setLayout(layout);
+		layout.setAutoCreateContainerGaps(true);
+		layout.setHorizontalGroup(layout.createSequentialGroup()
+				.addComponent(inputField)
+				.addComponent(enterButton)
+		);
+		layout.setVerticalGroup(layout.createParallelGroup(Alignment.BASELINE)
+				.addComponent(inputField)
+				.addComponent(enterButton)
+		);
+		this.add(inputFieldPanel, BorderLayout.SOUTH);
+		
+		System.setOut(new PrintStream(new OutputStream() {
+			
+			@Override
+			public void write(int c) {
+				console.setText(console.getText() + String.valueOf((char) c));
+			}
+		}));
+	
+	}
+	
+	
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new ConsoleView().setVisible(true);
+			}
+		});
+	}
+
+	
+	
+	
+	
+	
+	
+	
+}
