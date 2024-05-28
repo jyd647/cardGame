f23fc7a690141a152df79743e81cd1dc3d9beb5c
Tiffany <Tiffany@LAP-5BPK7HJ>
2024-05-27 23:49:29 -0700

    finished main method(?)

--- killer13/src/killer13/Killer.java
+++ killer13/src/killer13/Killer.java
@@ -131,22 +131,18 @@ public class Killer {
 	}
 	System.out.println("Are you sure you want to play this? Y/N");
 	String input2 = scanner.nextLine();
-	if (didAllPass()) 
-	{
+	if (didAllPass()) {
 		currentPlay.resetCombo();
 	}
-	if (currentPlay.combos().size() == 0 && input2.toUpperCase().equals("Y")) 
-	{
-		if (player.type().equals("none")) 
-		{
+	if (currentPlay.combos().size() == 0 && input2.toUpperCase().equals("Y")) {
+		if (player.type().equals("none")) {
 			System.out.println("Retry, not a valid combo");
 			player.printCombos();
 			System.out.println("");
 			playerTurn(player);
 			return;
 		} 
-		else 
-		{
+		else {
 			for (int i = 0; i < player.combos().size(); i++) 
 			{
 				for (int j = 0; j < player.totalCards().size(); j++) 
@@ -165,18 +161,12 @@ public class Killer {
 			counter = 0;
 		}
 	} 
-	else if (input2.toUpperCase().equals("Y")) 
-	{
-		if (player.type().equals(currentPlay.type())) 
-		{
-			if (player.combos().get(player.combos().size() - 1).overallRank() > currentPlay.combos().get(currentPlay.combos().size() - 1).overallRank()) 
-			{
-				for (int i = 0; i < player.combos().size(); i++) 
-				{
-					for (int j = 0; j < player.totalCards().size(); j++) 
-					{
-						if (player.combos().get(i).equals(player.totalCards().get(j))) 
-						{
+	else if (input2.toUpperCase().equals("Y")) {
+		if (player.type().equals(currentPlay.type())) {
+			if (player.combos().get(player.combos().size() - 1).overallRank() > currentPlay.combos().get(currentPlay.combos().size() - 1).overallRank()) {
+				for (int i = 0; i < player.combos().size(); i++) {
+					for (int j = 0; j < player.totalCards().size(); j++) {
+						if (player.combos().get(i).equals(player.totalCards().get(j))) {
 							player.totalCards().remove(j);
 						}
 					}
@@ -188,22 +178,17 @@ public class Killer {
 				System.out.println("");
 				counter = 0;
 			} 
-			else 
-			{
+			else {
 				System.out.println("Retry, combo is lower tier than last play.");
 				System.out.println("");
 				playerTurn(player);
 				return;
 			}
 		} 
-		else if ((player.type().substring(player.type().length() - 4, player.type().length()).equals("bomb") || player.type().equals("4 of a Kind")) && currentPlay.type().equals("1 of a Kind")) 
-		{
-			for (int i = 0; i < player.combos().size(); i++) 
-			{
-				for (int j = 0; j < player.totalCards().size(); j++) 
-				{
-					if (player.combos().get(i).equals(player.totalCards().get(j)))
-					{
+		else if ((player.type().substring(player.type().length() - 4, player.type().length()).equals("bomb") || player.type().equals("4 of a Kind")) && currentPlay.type().equals("1 of a Kind")) {
+			for (int i = 0; i < player.combos().size(); i++) {
+				for (int j = 0; j < player.totalCards().size(); j++) 	{
+					if (player.combos().get(i).equals(player.totalCards().get(j))){
 						player.totalCards().remove(j);
 					}
 				}
@@ -215,44 +200,37 @@ public class Killer {
 			System.out.println("");
 			counter = 0;
 		} 
-		else if (player.combos().size() == 0) 
-		{
+		else if (player.combos().size() == 0) {
 			System.out.println("You passed.");
 			System.out.println("Current pile: " + currentPlay.combos());
 			System.out.println("");
 			counter++;
 		} 
-		else 
-		{
+		else {
 			System.out.println("Retry, combo does not match previous play.");
 			playerTurn(player);
 			return;
 		}
 	} 
-	else if (input2.toUpperCase().equals("N")) 
-	{
+	else if (input2.toUpperCase().equals("N")) {
 		playerTurn(player);
 		return;
 	} 
-	else 
-	{
+	else {
 		System.out.println("Type in Y/N, retry.");
 		playerTurn(player);
 		return;
 	}
 }
 	
-	private static void compTurn(Hand comp) 
-	{
-		if (didAllPass()) 
-		{
+	private static void compTurn(Hand comp) {
+		if (didAllPass()) {
 			currentPlay.resetCombo();
 		}
 		comp.combos().clear();
 		comp.sortAll();
 		// Following 'if' executes if the computer has the first turn.
-		if (currentPlay.combos().size() == 0 && comp.totalCards().size() != 0) 
-		{
+		if (currentPlay.combos().size() == 0 && comp.totalCards().size() != 0) {
 			comp.addToCombo(comp.totalCards().get(0));
 			comp.totalCards().remove(0);
 			currentPlay.resetCombo();
@@ -261,22 +239,16 @@ public class Killer {
 			System.out.println("Computer " + totalHands.indexOf(comp) + " currently has " + comp.totalCards().size() + " cards remaining.");
 			System.out.println("Current pile: " + currentPlay.combos());
 			counter = 0;
-		} 
-		else 
-		{
+		} else {
 			String typeToBeat = currentPlay.type();
 			Card cardToBeat = currentPlay.combos().get(currentPlay.combos().size() - 1);
 			boolean played = false;
-			if (typeToBeat.length() >= 11 && typeToBeat.substring(typeToBeat.length() - 10).equals(" of a Kind")) 
-			{
+			if (typeToBeat.length() >= 11 && typeToBeat.substring(typeToBeat.length() - 10).equals(" of a Kind")) {
 				int ofAKind = Integer.parseInt(typeToBeat.substring(0, typeToBeat.length() - 10));
 				int ofAKindCounter = 1;
-				for (int i = 0; i < comp.totalCards().size() - 1; i++) 
-				{
-					if (ofAKind == 1) 
-					{
-						if (comp.totalCards().get(i).overallRank() > cardToBeat.overallRank()) 
-						{
+				for (int i = 0; i < comp.totalCards().size() - 1; i++) {
+					if (ofAKind == 1) {
+						if (comp.totalCards().get(i).overallRank() > cardToBeat.overallRank()) {
 							comp.addToCombo(comp.totalCards().get(i));
 							played = true;
 							comp.totalCards().remove(i);
@@ -288,8 +260,7 @@ public class Killer {
 							System.out.println("Current pile: " + currentPlay.combos());
 							break;
 						} 
-						else if (comp.totalCards().get(comp.totalCards().size() - 1).overallRank() > cardToBeat.overallRank()) 
-						{
+						else if (comp.totalCards().get(comp.totalCards().size() - 1).overallRank() > cardToBeat.overallRank()) {
 							comp.addToCombo(comp.totalCards().get(comp.totalCards().size() - 1));
 							played = true;
 							comp.totalCards().remove(comp.totalCards().size() - 1);
@@ -301,26 +272,19 @@ public class Killer {
 							System.out.println("Current pile: " + currentPlay.combos());
 							break;
 						}
-						else
-						{
+						else{
 							
 						}
 					} 
-					else if (comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 1).getRank()) 
-					{
+					else if (comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 1).getRank()) {
 						comp.addToCombo(comp.totalCards().get(i));
 						ofAKindCounter++;
-						if (ofAKindCounter == ofAKind) 
-						{
+						if (ofAKindCounter == ofAKind)	{
 							comp.addToCombo(comp.totalCards().get(i + 1));
-							if (comp.combos().get(comp.combos().size() - 1).overallRank() > currentPlay.combos().get(currentPlay.combos().size() - 1).overallRank()) 
-							{
-								for (int j = 0; j < comp.combos().size(); j++) 
-								{
-									for (int k = 0; k < comp.totalCards().size(); k++) 
-									{
-										if (comp.combos().get(j).equals(comp.totalCards().get(k))) 
-										{
+							if (comp.combos().get(comp.combos().size() - 1).overallRank() > currentPlay.combos().get(currentPlay.combos().size() - 1).overallRank()) {
+								for (int j = 0; j < comp.combos().size(); j++) {
+									for (int k = 0; k < comp.totalCards().size(); k++) {
+										if (comp.combos().get(j).equals(comp.totalCards().get(k))) {
 											comp.totalCards().remove(k);
 										}
 									}
@@ -336,57 +300,42 @@ public class Killer {
 							}
 						}
 					} 
-					else 
-					{
+					else {
 						ofAKindCounter = 1;
 						comp.resetCombo();
 					}
 				}
-				if(!played)
-				{
-					for(int a = 2; a < 5; a++)
-					{
-						for(int b = 3; b < 7; b++)
-						{
+				if(!played){
+					for(int a = 2; a < 5; a++){
+						for(int b = 3; b < 7; b++){
 							int bombType = a;
 							int bombLength = b;
 							int bombCounter = 0;
-							if(bombType == 2)
-							{
-								for(int i = 0; i < comp.totalCards().size() - 1; i++)
-								{
-									if(comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 1).getRank())
-									{
-										if(i >= 1 && comp.totalCards().get(i).getRank() == comp.totalCards().get(i - 1).getRank() + 1)
-										{
+							if(bombType == 2){
+								for(int i = 0; i < comp.totalCards().size() - 1; i++){
+									if(comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 1).getRank()){
+										if(i >= 1 && comp.totalCards().get(i).getRank() == comp.totalCards().get(i - 1).getRank() + 1){
 											bombCounter++;
 											comp.addToCombo(comp.totalCards().get(i));
 											comp.addToCombo(comp.totalCards().get(i + 1));
 											i++;
 										}
-										else if(i < 1)
-										{
+										else if(i < 1){
 											bombCounter++;
 											comp.addToCombo(comp.totalCards().get(i));
 											comp.addToCombo(comp.totalCards().get(i + 1));
 											i++;
 										}
 									}
-									else if(i >= 1 && comp.totalCards().get(i).getRank() != comp.totalCards().get(i + 1).getRank() && comp.totalCards().get(i).getRank() != comp.totalCards().get(i - 1).getRank())
-									{
+									else if(i >= 1 && comp.totalCards().get(i).getRank() != comp.totalCards().get(i + 1).getRank() && comp.totalCards().get(i).getRank() != comp.totalCards().get(i - 1).getRank()){
 										bombCounter = 0;
 										comp.resetCombo();
 									}
-									if(bombCounter == bombLength)
-									{
-										if (comp.combos().get(comp.combos().size() - 1).overallRank() > currentPlay.combos().get(currentPlay.combos().size() - 1).overallRank()) 
-										{
-											for (int j = 0; j < comp.combos().size(); j++) 
-											{
-												for (int k = 0; k < comp.totalCards().size(); k++) 
-												{
-													if (comp.combos().get(j).equals(comp.totalCards().get(k))) 
-													{
+									if(bombCounter == bombLength){
+										if (comp.combos().get(comp.combos().size() - 1).overallRank() > currentPlay.combos().get(currentPlay.combos().size() - 1).overallRank()) {
+											for (int j = 0; j < comp.combos().size(); j++) {
+												for (int k = 0; k < comp.totalCards().size(); k++) {
+													if (comp.combos().get(j).equals(comp.totalCards().get(k))) {
 														comp.totalCards().remove(k);
 													}
 												}
@@ -403,22 +352,17 @@ public class Killer {
 									}
 								}
 							}
-							else if(bombType == 3)
-							{
-								for(int i = 0; i < comp.totalCards().size() - 2; i++)
-								{
-									if(comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 1).getRank() && comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 2).getRank())
-									{
-										if(i >= 1 && comp.totalCards().get(i).getRank() == comp.totalCards().get(i - 1).getRank() + 1)
-										{
+							else if(bombType == 3){
+								for(int i = 0; i < comp.totalCards().size() - 2; i++)	{
+									if(comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 1).getRank() && comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 2).getRank()){
+										if(i >= 1 && comp.totalCards().get(i).getRank() == comp.totalCards().get(i - 1).getRank() + 1){
 											bombCounter++;
 											comp.addToCombo(comp.totalCards().get(i));
 											comp.addToCombo(comp.totalCards().get(i + 1));
 											comp.addToCombo(comp.totalCards().get(i + 2));
 											i += 2;
 										}
-										else if(i < 1)
-										{
+										else if(i < 1){
 											bombCounter++;
 											comp.addToCombo(comp.totalCards().get(i));
 											comp.addToCombo(comp.totalCards().get(i + 1));
@@ -426,27 +370,19 @@ public class Killer {
 											i += 2;
 										}
 									}
-									else if(i >= 2 && i <= comp.totalCards().size() - 3)
-									{
-										if(comp.totalCards().get(i).getRank() != comp.totalCards().get(i + 1).getRank() && comp.totalCards().get(i).getRank() != comp.totalCards().get(i - 1).getRank())
-										{
-											if(comp.totalCards().get(i).getRank() != comp.totalCards().get(i + 2).getRank() && comp.totalCards().get(i).getRank() != comp.totalCards().get(i - 2).getRank())
-											{
+									else if(i >= 2 && i <= comp.totalCards().size() - 3)	{
+										if(comp.totalCards().get(i).getRank() != comp.totalCards().get(i + 1).getRank() && comp.totalCards().get(i).getRank() != comp.totalCards().get(i - 1).getRank()){
+											if(comp.totalCards().get(i).getRank() != comp.totalCards().get(i + 2).getRank() && comp.totalCards().get(i).getRank() != comp.totalCards().get(i - 2).getRank()){
 												bombCounter = 0;
 												comp.resetCombo();
 											}
 										}
 									}
-									if(bombCounter == bombLength)
-									{
-										if (comp.combos().get(comp.combos().size() - 1).overallRank() > currentPlay.combos().get(currentPlay.combos().size() - 1).overallRank()) 
-										{
-											for (int j = 0; j < comp.combos().size(); j++) 
-											{
-												for (int k = 0; k < comp.totalCards().size(); k++) 
-												{
-													if (comp.combos().get(j).equals(comp.totalCards().get(k))) 
-													{
+									if(bombCounter == bombLength){
+										if (comp.combos().get(comp.combos().size() - 1).overallRank() > currentPlay.combos().get(currentPlay.combos().size() - 1).overallRank()) {
+											for (int j = 0; j < comp.combos().size(); j++) {
+												for (int k = 0; k < comp.totalCards().size(); k++) {
+													if (comp.combos().get(j).equals(comp.totalCards().get(k))) {
 														comp.totalCards().remove(k);
 													}
 												}
@@ -463,14 +399,10 @@ public class Killer {
 									}
 								}
 							}
-							else if(bombType == 4)
-							{
-								for(int i = 0; i < comp.totalCards().size() - 3; i++)
-								{
-									if(comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 1).getRank() && comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 2).getRank() && comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 3).getRank())
-									{
-										if(i >= 1 && comp.totalCards().get(i).getRank() == comp.totalCards().get(i - 1).getRank() + 1)
-										{
+							else if(bombType == 4){
+								for(int i = 0; i < comp.totalCards().size() - 3; i++){
+									if(comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 1).getRank() && comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 2).getRank() && comp.totalCards().get(i).getRank() == comp.totalCards().get(i + 3).getRank()){
+										if(i >= 1 && comp.totalCards().get(i).getRank() == comp.totalCards().get(i - 1).getRank() + 1){
 											bombCounter++;
 											comp.addToCombo(comp.totalCards().get(i));
 											comp.addToCombo(comp.totalCards().get(i + 1));
@@ -478,8 +410,7 @@ public class Killer {
 											comp.addToCombo(comp.totalCards().get(i + 3));
 											i += 2;
 										}
-										else if(i < 1)
-										{
+										else if(i < 1){
 											bombCounter++;
 											comp.addToCombo(comp.totalCards().get(i));
 											comp.addToCombo(comp.totalCards().get(i + 1));
@@ -813,31 +744,26 @@ public class Killer {
 		d.shuffle();
 		Hand player = new Hand();
 		totalHands.add(player);
-		while (true) 
-		{
+		while (true) {
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("Welcome to Killer!");
 			System.out.println("How many players? (2-4)");
 			String string = scanner.nextLine();
-			if (string.equals("2")) 
-			{
+			if (string.equals("2")) {
 				Hand comp1 = new Hand();
 				totalHands.add(comp1);
-				for (int i = 0; i < 13; i++) 
-				{
+				for (int i = 0; i < 13; i++) {
 					player.insertCard(d.deal());
 					comp1.insertCard(d.deal());
 				}
 				break;
 			} 
-			else if (string.equals("3")) 
-			{
+			else if (string.equals("3")) {
 				Hand comp1 = new Hand();
 				totalHands.add(comp1);
 				Hand comp2 = new Hand();
 				totalHands.add(comp2);
-				for (int i = 0; i < 13; i++) 
-				{
+				for (int i = 0; i < 13; i++) {
 					player.insertCard(d.deal());
 					comp1.insertCard(d.deal());
 					comp2.insertCard(d.deal());
@@ -845,16 +771,14 @@ public class Killer {
 				break;
 			} 
 			
-			else if (string.equals("4")) 
-			{
+			else if (string.equals("4")) {
 				Hand comp1 = new Hand();
 				totalHands.add(comp1);
 				Hand comp2 = new Hand();
 				totalHands.add(comp2);
 				Hand comp3 = new Hand();
 				totalHands.add(comp3);
-				for (int i = 0; i < 13; i++) 
-				{
+				for (int i = 0; i < 13; i++) {
 					player.insertCard(d.deal());
 					comp1.insertCard(d.deal());
 					comp2.insertCard(d.deal());
@@ -862,10 +786,33 @@ public class Killer {
 				}
 				break;
 			} 
-			else 
-			{
+			else {
 				System.out.println("Retry");
 			}
+		}
+		
+		int index = totalHands.indexOf(firstTurn(totalHands));
+		while (isGameStillRunning()) 
+		{
+			if (index == 0) {
+				playerTurn(player);
+			} 
+			else {
+				compTurn(totalHands.get(index));
+			}
+			if (index == totalHands.size() - 1) {
+				index = -1;
+			}
+			index++;
+		}
+		if(index == 1) {
+			System.out.println("Congratulations! You are the winner!");
+		}
+		else if(index == 0){
+			System.out.println("Computer " + (totalHands.size() - 1) + " has won! You lost...");
+		}
+		else {
+			System.out.println("Computer " + (index - 1) + " has won! You lost...");
 		}
 
 		
